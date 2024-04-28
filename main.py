e2febe88bb23dfd6044b68e6152f6dbd57eec37a
KashishGulati <kgulati100@gmail.com>
2024-04-28 23:53:39 +0530

    Initial Commit.

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, send_from_directory
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/download_resume')
+def download_resume():
+    # Send the file from the 'static' folder with attachment disposition
+    return send_from_directory('static', 'files/Kashish_resume_04.pdf', as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
